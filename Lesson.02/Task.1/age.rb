32a41d39d58b7776d5eed670f4767d9b5d516d64
Digital-Advance <mcqueen1395@mail.ru>
2022-11-28 15:28:02 +0300

    Added the loop and screenshot

--- Lesson.02/Task.1/age.rb
+++ Lesson.02/Task.1/age.rb
@@ -1,30 +1,35 @@
 print "Компания 'CDG' приветствует Вас. Для продолжения нажмите 'Enter'."
   gets.chomp
-puts "Ведите Ваше имя:"
-  @name = gets.chomp
-puts "Введите Вашу фамилию:"
-  @surname = gets.chomp
-puts "Введите Ваш возраст:"
-  @age = gets.chomp.to_i
 
-@difference_age =  18 - @age 
+loop do
+  puts "Ведите Ваше имя:"
+    @name = gets.chomp
+  puts "Введите Вашу фамилию:"
+    @surname = gets.chomp
+  puts "Введите Ваш возраст:"
+    @age = gets.chomp.to_i
 
-def gradient_age
-  if @difference_age == 1
-    return "год"
-  elsif @difference_age == 2
-    return "года"
-  elsif @difference_age == 3
-    return "года"
-  elsif @difference_age == 4
-    return "года"
-  else
-    return "лет"
+  @difference_age =  18 - @age 
+
+  def gradient_age
+    if @difference_age == 1
+      return "год"
+    elsif @difference_age == 2
+      return "года"
+    elsif @difference_age == 3
+      return "года"
+    elsif @difference_age == 4
+      return "года"
+    else
+      return "лет"
   end
 end
 
-def pass
-  return @age >= 18 ? ("Здравствуйте, #{@name} #{@surname}. Самое время заняться делом.") : ("Здравствуйте, #{@name} #{@surname}. Вам меньше 18 лет, но начать учиться программированию никогда не рано. Усердно учитесь и через #{@difference_age} #{gradient_age} когда Вам исполнится 18 лет, Вы сможете заняться делом.")
+  def pass
+    return @age >= 18 ? ("Здравствуйте, #{@name} #{@surname}. Самое время заняться делом.") : ("Здравствуйте, #{@name} #{@surname}. Вам меньше 18 лет, но начать учиться программированию никогда не рано. Усердно учитесь и через #{@difference_age} #{gradient_age} когда Вам исполнится 18 лет, Вы сможете заняться делом.")
 end
 
-puts pass
+  puts "#{pass} \n\nДля завершения программы введите 'Стоп' и нажмите 'Enter'. Для ввода новых данных нажмите 'Enter':"
+  input = gets.chomp.downcase
+  break if input == "стоп"
+end
