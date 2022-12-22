e4de34973e639aa4dc9c94d2ea678d35635020f7
Digital-Advance <mcqueen1395@mail.ru>
2022-12-22 15:04:53 +0300

    Added append method

---
+++ Lesson.04/Task.2/student.rb
@@ -0,0 +1,47 @@
+require 'pry'
+
+STUDENT_LIST = 'student_list.txt'
+RESULTS = 'results.txt'
+
+puts "\nФайл успешно загружен. Для получения списка студентов нажми 'Enter'.".rjust(146, "_")
+  gets.chomp
+
+def index
+  puts "\nСтуденты в списке:\n".center(172, "=")
+    file = File.open(STUDENT_LIST)
+      @arr = file.readlines.map { |x| x.chomp }
+    File.foreach(STUDENT_LIST) { |student| puts "> #{student}" }
+end
+
+puts "#{index}"
+
+def select_function
+  loop do
+    puts "\nВыбери необходимую функцию:\n".center(179, "=")
+    print">1 - Добавить студента в конец списка.\n>2 - Отсортировать студентов по возрасту.\n>3 - Удалить студента из списка по ID.\n>4 - Завершить программу.\nВвод >> "
+    function = gets.chomp.to_i
+    if function == 1
+      def add_list
+        print "\nВведи фамилию студента >> ".rjust(103, "-")
+          add_surname = gets.chomp
+        print "\nВведи имя студента >> "
+          add_name = gets.chomp
+        print "\nВведи отчество студента >> "
+          add_patronymic = gets.chomp
+        print "\nВведи возраст студента >> "
+          add_age = gets.chomp
+        add_student = "#{add_surname} #{add_name} #{add_patronymic} #{add_age}"
+        File.write(STUDENT_LIST, "#{add_student}\n", mode: "a")
+          puts "\nСтудент добавлен. Список обновлен:\n".center(189, "=")
+        File.foreach(STUDENT_LIST) { |student| puts "> #{student}" }
+      end
+      puts add_list
+
+    elsif function == 4
+      abort "\nПрограмма завершена\n".center(171, "=")
+    break if function == 2
+    end
+  end
+end
+
+puts select_function
