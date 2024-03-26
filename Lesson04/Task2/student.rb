434a8b8786bb036c7556f535cefe6fcb973f211d
Digital-Advance <mcqueen1395@mail.ru>
2024-03-26 23:12:08 +0300

    Added find method and update db.txt

--- Lesson04/Task2/student.rb
+++ Lesson04/Task2/student.rb
@@ -1,12 +1,20 @@
 STUDENT = 'db.txt'
 RESULT = 'result.txt'
-a = []
 
 def exit_programm
   abort "Работа с программой завершена"
 end
 
 def read_file
-  File.open(STUDENT).readlines.map(&:chomp)
+  file = File.open(STUDENT).readlines.map(&:chomp)
 end
-  
+
+def find
+  print "Введите возраст для поиска студента: >> "
+    age = gets.chomp
+      find_age = read_file.select { |value| value.include? age }
+    puts "Студенты с возрастом #{age}:\n\nID     Студент     Возраст"
+  new_list = find_age.each_with_index { |value, index| puts "#{index}| #{value}" }
+end
+
+find
