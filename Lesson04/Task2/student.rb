f546080fc55515f2540d2bd8ce3268f51bb17e68
Digital-Advance <mcqueen1395@mail.ru>
2024-04-06 16:08:22 +0300

    Added move_student method

--- Lesson04/Task2/student.rb
+++ Lesson04/Task2/student.rb
@@ -1,12 +1,13 @@
 STUDENT = 'db.txt'
 RESULT = 'result.txt'
+BUFFER = 'buffer.txt'
 
 def exit_programm
   abort "Работа с программой завершена"
 end
 
 def read_file
-  file = File.open(STUDENT).readlines.map(&:chomp)
+  File.open(STUDENT).readlines.map(&:chomp)
 end
 
 def find
@@ -15,6 +16,24 @@ def find
       find_age = read_file.select { |value| value.include? age }
     puts "Студенты с возрастом #{age}:\n\nID     Студент     Возраст"
   new_list = find_age.each_with_index { |value, index| puts "#{index}| #{value}" }
+end
+
+def move_student
+  print "Введите ID студента для записи его в отдельный список: >> "
+    input_id = gets.to_i
+
+    File.open(RESULT, "a") { |new| new.write "#{read_file[input_id]}\n" }
+
+      list = read_file
+        delete_student = read_file[input_id]
+      list.slice!(input_id)
+      
+      file = File.open(BUFFER, "w")
+        file.write("#{list.join("\n")}")
+      file.close
+  
+    File.write(STUDENT, File.read(BUFFER))
+  File.delete(BUFFER) if File.exist?(BUFFER)
 end
 
 find
