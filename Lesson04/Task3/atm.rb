ea7b009dd79a3b441901e858e81cfbc8c26f77f2
Digital-Advance <mcqueen1395@mail.ru>
2024-04-07 17:25:57 +0300

    Added error message if input sum = 0 and added comments to code

--- Lesson04/Task3/atm.rb
+++ Lesson04/Task3/atm.rb
@@ -1,53 +1,57 @@
-BALANCE = 'balance.txt'
-BUFFER = 'buffer.txt'
+BALANCE = 'balance.txt'  # Имя файла для хранения баланса
+BUFFER = 'buffer.txt'    # Временный файл для обновления баланса
 
 def balance
-  File.open(BALANCE).read.to_f
+  File.open(BALANCE).read.to_f  # Функция для чтения текущего баланса из файла
 end
 
 def show_balance
-  puts "\nБаланс счета = #{balance} ₽."
+  puts "\nБаланс счета = #{balance} ₽."  # Вывод текущего баланса на экран
 end
 
 def add_cash
     print "\nВведите сумму для пополнения: >> "
-  input_cash = gets.to_f
+  input_cash = gets.to_f  # Получение суммы для пополнения от пользователя
     if input_cash.negative?
       puts "\nСумма не может быть отрицательной."
+    elsif input_cash.zero?
+      puts "\nСумма не может быть нулевой."
     else
-      new_balance = balance + input_cash
-    file = File.open(BUFFER, "w")
-      file.write(new_balance)
-  
+      new_balance = balance + input_cash  # Вычисление нового баланса после пополнения
+    file = File.open(BUFFER, "w")  # Открытие временного файла для записи нового баланса
+      file.write(new_balance)  # Запись нового баланса во временный файл
+
     file.close
-      File.write(BALANCE, File.read(BUFFER))
-    File.delete(BUFFER) if File.exist?(BUFFER)
+      File.write(BALANCE, File.read(BUFFER))  # Обновление файла с балансом
+    File.delete(BUFFER) if File.exist?(BUFFER)  # Удаление временного файла
   end
 end
 
 def withdraw_cash
     print "\nВведите сумму для снятия: >> "
-  withdraw_cash = gets.to_f
+  withdraw_cash = gets.to_f  # Получение суммы для снятия от пользователя
     if withdraw_cash > balance
       puts "\nНедостаточно средств."
     elsif withdraw_cash.negative?
       puts "\nСумма не может быть отрицательной."
+    elsif withdraw_cash.zero?
+      puts "\nСумма не может быть нулевой."
     else
-      new_balance = balance - withdraw_cash
-    file = File.open(BUFFER, "w")
-      file.write(new_balance)
+      new_balance = balance - withdraw_cash  # Вычисление нового баланса после снятия
+    file = File.open(BUFFER, "w")  # Открытие временного файла для записи нового баланса
+      file.write(new_balance)  # Запись нового баланса во временный файл
 
     file.close
-      File.write(BALANCE, File.read(BUFFER))
-    File.delete(BUFFER) if File.exist?(BUFFER)
+      File.write(BALANCE, File.read(BUFFER))  # Обновление файла с балансом
+    File.delete(BUFFER) if File.exist?(BUFFER)  # Удаление временного файла
   end
 end
 
 def menu
   loop do
     puts "\nВыберите действие:\n\n1. Добавить деньги.\n2. Снять деньги.\n3. Показать баланс.\n4. Выход."
-     print "\nВвод: >> "
-  action = gets.to_i
+      print "\nВвод: >> "
+    action = gets.to_i  # Получение выбора действия от пользователя
     case action
     when 1
       add_cash
@@ -56,9 +60,9 @@ def menu
     when 3
       show_balance
     when 4
-      abort "Программа завершена"
+      abort "Программа завершена"  # Завершение программы
     end
   end
 end
 
-menu
+menu  # Вызов функции для запуска программы
