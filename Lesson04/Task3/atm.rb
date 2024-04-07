40fea58484f746b2224fbfc40cee150d24218cbd
Digital-Advance <mcqueen1395@mail.ru>
2024-04-07 16:38:22 +0300

    Added method add_balance

--- Lesson04/Task3/atm.rb
+++ Lesson04/Task3/atm.rb
@@ -8,3 +8,14 @@ end
 def show_balance
   puts "Баланс счета = #{balance}"
 end
+
+def add_balance
+  input_cash = gets.to_f
+    sum_balance = balance + input_cash
+  file = File.open(BUFFER, "w")
+    file.write(new_balance)
+  
+  file.close
+    File.write(BALANCE, File.read(BUFFER))
+  File.delete(BUFFER) if File.exist?(BUFFER)
+end
