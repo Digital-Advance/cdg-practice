8087a95229e92dfa3e9cb5c0326b80b19a0ef0d7
Digital-Advance <mcqueen1395@mail.ru>
2024-04-07 17:02:00 +0300

    Added withdraw_cash and menu methods

--- Lesson04/Task3/atm.rb
+++ Lesson04/Task3/atm.rb
@@ -6,16 +6,59 @@ def balance
 end
 
 def show_balance
-  puts "Баланс счета = #{balance}"
+  puts "\nБаланс счета = #{balance} ₽."
 end
 
-def add_balance
+def add_cash
+    print "\nВведите сумму для пополнения: >> "
   input_cash = gets.to_f
-    sum_balance = balance + input_cash
-  file = File.open(BUFFER, "w")
-    file.write(new_balance)
+    if input_cash.negative?
+      puts "\nСумма не может быть отрицательной."
+    else
+      new_balance = balance + input_cash
+    file = File.open(BUFFER, "w")
+      file.write(new_balance)
   
-  file.close
-    File.write(BALANCE, File.read(BUFFER))
-  File.delete(BUFFER) if File.exist?(BUFFER)
+    file.close
+      File.write(BALANCE, File.read(BUFFER))
+    File.delete(BUFFER) if File.exist?(BUFFER)
+  end
 end
+
+def withdraw_cash
+    print "\nВведите сумму для снятия: >> "
+  withdraw_cash = gets.to_f
+    if withdraw_cash > balance
+      puts "\nНедостаточно средств."
+    elsif withdraw_cash.negative?
+      puts "\nСумма не может быть отрицательной."
+    else
+      new_balance = balance - withdraw_cash
+    file = File.open(BUFFER, "w")
+      file.write(new_balance)
+
+    file.close
+      File.write(BALANCE, File.read(BUFFER))
+    File.delete(BUFFER) if File.exist?(BUFFER)
+  end
+end
+
+def menu
+  loop do
+    puts "\nВыберите действие:\n\n1. Добавить деньги.\n2. Снять деньги.\n3. Показать баланс.\n4. Выход."
+     print "\nВвод: >> "
+  action = gets.to_i
+    case action
+    when 1
+      add_cash
+    when 2
+      withdraw_cash
+    when 3
+      show_balance
+    when 4
+      abort "Программа завершена"
+    end
+  end
+end
+
+menu
